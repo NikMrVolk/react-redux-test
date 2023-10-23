a8ce666b92f9ab6f473e445837627e131210cb81
NickZavadski <nick.mr.volk@gmail.com>
2023-10-23 12:44:05 +0300

    add logic with number state

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,53 @@
+import { useDispatch, useSelector } from 'react-redux'
 import './App.css'
 
 function App() {
-	return <div className="App">
-		<div style={{display: 'flex'}}>
-			<button>Пополнить счёт</button>
-			<button>Снять со счёта</button>
+	const dispatch = useDispatch()
+	const cash = useSelector((state) => state.cash)
+
+	const addCash = (moneyValue) => {
+		dispatch({
+			type: 'ADD_CASH',
+			payload: moneyValue,
+		})
+	}
+
+	const getCash = (moneyValue) => {
+		dispatch({
+			type: 'GET_CASH',
+			payload: moneyValue,
+		})
+	}
+
+	return (
+		<div className="App">
+			<div
+				style={{
+					display: 'flex',
+					height: '100vh',
+					justifyContent: 'center',
+					alignItems: 'center',
+					gap: '15px',
+				}}
+			>
+				<button
+					onClick={() => {
+						addCash(+prompt('Your value'))
+					}}
+				>
+					Пополнить счёт
+				</button>
+				<button
+					onClick={() => {
+						getCash(+prompt('Your value'))
+					}}
+				>
+					Снять со счёта
+				</button>
+				{cash}
+			</div>
 		</div>
-	</div>
+	)
 }
 
 export default App
--- src/index.js
+++ src/index.js
@@ -1,8 +1,31 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { Provider } from 'react-redux'
+import { configureStore } from '@reduxjs/toolkit'
+import App from './App'
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const defaultState = {
+	cash: 5,
+}
+
+const reducer = (state = defaultState, action) => {
+	switch (action.type) {
+		case 'ADD_CASH':
+			return { ...state, cash: state.cash + action.payload }
+		case 'GET_CASH':
+			return { ...state, cash: state.cash - action.payload }
+		default:
+			return state
+	}
+}
+
+const store = configureStore({
+	reducer: reducer,
+})
+
+const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
-    <App />
-);
+	<Provider store={store}>
+		<App />
+	</Provider>
+)
