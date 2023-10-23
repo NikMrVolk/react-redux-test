c41e9f6c504a00c1bbd843d43d640aabfd12f773
NickZavadski <nick.mr.volk@gmail.com>
2023-10-23 10:51:35 +0300

    some prepares and initial dependencies

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+	return <div className="App">
+		<div style={{display: 'flex'}}>
+			<button>Пополнить счёт</button>
+			<button>Снять со счёта</button>
+		</div>
+	</div>
+}
+
+export default App
